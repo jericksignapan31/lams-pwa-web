070cf51a5d4c2155cceb4340933ed69760cf9742
Jerick Signapan <jericksignapan3331@gmail.com>
2025-06-10 16:59:02 +0800

    feat: :sparkles: refactor dashbaord

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,14 @@
 // app.routes.ts
 import { Routes } from '@angular/router';
-import { homeRoute } from './modules/home/home/home.route';
+import { homeRoute } from './modules/home/home.route';
 
 export const routes: Routes = [
   {
     path: 'login',
     loadComponent: () =>
-      import('./modules/auth/login/login.component').then(m => m.LoginComponent),
+      import('./modules/auth/login/login.component').then(
+        (m) => m.LoginComponent
+      ),
     title: 'Login',
   },
   {
@@ -17,7 +19,7 @@ export const routes: Routes = [
   {
     path: 'home',
     loadComponent: () =>
-      import('./modules/home/home/home.component').then(m => m.HomeComponent),
+      import('./modules/home/home.component').then((m) => m.HomeComponent),
     children: homeRoute,
-  }
+  },
 ];
