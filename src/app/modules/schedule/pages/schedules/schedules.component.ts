3f3783dee759203dcfd5f7c6588fbea180f4b9f9
Jerick Signapan <jericksignapan3331@gmail.com>
2025-06-21 18:55:09 +0800

    add table in sched

--- src/app/modules/schedule/pages/schedules/schedules.component.ts
+++ src/app/modules/schedule/pages/schedules/schedules.component.ts
@@ -12,10 +12,19 @@ import { AddScheduleComponent } from '../../components/add-schedule/add-schedule
   providers: [AddScheduleComponent],
 })
 export class SchedulesComponent implements OnInit {
+  laboratoryId: string = '';
   laboratoryName: string = '';
   roomName: string = '';
-  showTopDiv = true;
-
+  showTopDiv = false;
+  days = [
+    'Sunday',
+    'Monday',
+    'Tuesday',
+    'Wednesday',
+    'Thursday',
+    'Friday',
+    'Saturday',
+  ];
   form = {
     class_name: '',
     faculty: null,
@@ -30,8 +39,47 @@ export class SchedulesComponent implements OnInit {
 
   constructor(private route: ActivatedRoute) {}
 
+  timeSlots = [
+    '07:00 AM',
+    '07:30 AM',
+    '08:00 AM',
+    '08:30 AM',
+    '09:00 AM',
+    '09:30 AM',
+    '10:00 AM',
+    '10:30 AM',
+    '11:00 AM',
+    '11:30 AM',
+    '12:00 PM',
+    '12:30 PM',
+    '01:00 PM',
+    '01:30 PM',
+    '02:00 PM',
+    '02:30 PM',
+    '03:00 PM',
+    '03:30 PM',
+    '04:00 PM',
+    '04:30 PM',
+    '05:00 PM',
+    '05:30 PM',
+    '06:00 PM',
+    '06:30 PM',
+    '07:00 PM',
+    '07:30 PM',
+    '08:00 PM',
+    '08:30 PM',
+    '09:00 PM',
+    '09:30 PM',
+    '10:00 PM',
+    '10:30 PM',
+    '11:00 PM',
+    '11:30 PM',
+    '12:00 AM',
+  ];
+
   ngOnInit() {
     this.route.paramMap.subscribe((params) => {
+      this.laboratoryId = params.get('laboratoryId') || '';
       this.laboratoryName = params.get('laboratoryName') || '';
       this.roomName = params.get('roomName') || '';
       // You can fetch or display data for this laboratory/room here
